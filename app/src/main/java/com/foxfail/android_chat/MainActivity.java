1ba2a378a443f1c6c9e46041de60b88002b7d668
Foxfail <foxfail@mail.ru>
2019-10-05 22:36:53 +0300

    pkg android_chat, в котором будут лежать все классы связанные с чатом под android

--- app/src/main/java/com/foxfail/android_chat/MainActivity.java
+++ app/src/main/java/com/foxfail/android_chat/MainActivity.java
@@ -1,13 +1,15 @@
-package com.foxfail.training_netchat;
+package com.foxfail.android_chat;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.foxfail.training_netchat.R;
 
 public class MainActivity extends AppCompatActivity {
 
